c67356af3fb316e3358146e4e9d5a70b37eccc84
Dawid <dawidsobieszczuk@gmail.com>
2022-02-27 15:37:13 +0100

    API Authentication - Test update

--- tests/Feature/AuthTest.php
+++ tests/Feature/AuthTest.php
@@ -15,16 +15,18 @@ class AuthTest extends TestCase
     use RefreshDatabase;
 
     private const VERSION = 'v1';
+    private const USER_EMAIL = 'user@email.com';
+    private const USER_PASSWORD = 'password';
 
     public function test_login()
     {
         $this->seed(UserSeeder::class);
-        $userEmail = 'user@email.com';
-        $userPassword = 'password';
 
-        $this->json('POST', 'api/' . self::VERSION . '/login', [
-            'email' => $userEmail,
-            'password' => $userPassword,
+        $url = 'api/' . self::VERSION . '/login';
+
+        $this->postJson($url, [
+            'email' => self::USER_EMAIL,
+            'password' => self::USER_PASSWORD,
         ])
             ->assertValid()
             ->assertStatus(201)
@@ -35,55 +37,56 @@ class AuthTest extends TestCase
         $this->assertDatabaseCount('personal_access_tokens', 1);
 
 
-        $this->json('POST', 'api/' . self::VERSION . '/login', [
-            'email' => $userEmail . '0',
-            'password' => $userPassword,
+        $this->postJson($url, [
+            'email' => self::USER_EMAIL . '0',
+            'password' => self::USER_PASSWORD,
         ])
             ->assertValid()
             ->assertStatus(401)
             ->assertJson(fn (AssertableJson $json) => $json->has('message'));
 
-        $this->json('POST', 'api/' . self::VERSION . '/login', [
-            'email' => $userEmail,
-            'password' => $userPassword . '0',
+        $this->postJson($url, [
+            'email' => self::USER_EMAIL,
+            'password' => self::USER_PASSWORD . '0',
         ])
             ->assertValid()
             ->assertStatus(401)
             ->assertJson(fn (AssertableJson $json) => $json->has('message'));
 
-        $this->json('POST', 'api/' . self::VERSION . '/login', [
-            //'email' => $userEmail,
-            'password' => $userPassword,
+        $this->postJson($url, [
+            //'email' => self::USER_EMAIL,
+            'password' => self::USER_PASSWORD,
         ])->assertInvalid(['email']);
 
-        $this->json('POST', 'api/' . self::VERSION . '/login', [
+        $this->postJson($url, [
             'email' => 'email',
-            'password' => $userPassword,
+            'password' => self::USER_PASSWORD,
         ])->assertInvalid(['email']);
 
-        $this->json('POST', 'api/' . self::VERSION . '/login', [
-            'email' => $userEmail,
-            //'password' => $userPassword,
+        $this->postJson($url, [
+            'email' => self::USER_EMAIL,
+            //'password' => self::USER_PASSWORD,
         ])->assertInvalid(['password']);
     }
 
     public function test_logout()
     {
         $this->seed(UserSeeder::class);
-        $userEmail = 'user@email.com';
-        $userPassword = 'password';
 
-        $token = $this->postJson('api/' . self::VERSION . '/login', [
-            'email' => $userEmail,
-            'password' => $userPassword,
+        $urlLogin = 'api/' . self::VERSION . '/login';
+        $urlLogout = 'api/' . self::VERSION . '/logout';
+
+        $token = $this->postJson($urlLogin, [
+            'email' => self::USER_EMAIL,
+            'password' => self::USER_PASSWORD,
         ])->json()['token'];
         $this->assertDatabaseCount('personal_access_tokens', 1);
 
-        $this->getJson('api/' . self::VERSION . '/logout')
+        $this->getJson($urlLogout)
             ->assertStatus(401)
             ->assertJson(fn (AssertableJson $json) => $json->has('message'));
 
-        $this->withHeader('Authorization', 'Bearer ' . $token)->getJson('api/' . self::VERSION . '/logout')
+        $this->withHeader('Authorization', 'Bearer ' . $token)->getJson($urlLogout)
             ->assertStatus(200)
             ->assertJson(fn (AssertableJson $json) => $json->has('message'));
 
