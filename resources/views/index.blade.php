a9753f47ab38d3c09e859f4b3df18e3e792e94a2
Dawid <dawidsobieszczuk@gmail.com>
2022-08-03 11:03:53 +0200

    add support for angular

---
+++ resources/views/index.blade.php
@@ -0,0 +1,2 @@
+<?php
+    include __DIR__ . '/../../../public/ng/index.html';
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\StartController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -12,3 +13,6 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+
+Route::get('/', [StartController::class, 'index']);
+Route::get('/{route?}', [StartController::class, 'index'])->where('route', '(.*)');
