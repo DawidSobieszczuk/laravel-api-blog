6374c76ca49851dda117fc6f811b73bf52a71511
Dawid <dawidsobieszczuk@gmail.com>
2022-08-23 16:00:33 +0200

    fix - save corect file path to database

--- app/Services/FileService.php
+++ app/Services/FileService.php
@@ -1,36 +1,37 @@
 <?php
-    namespace App\Services;
+
+namespace App\Services;
 
 use App\Repositories\FileRepository;
-use Illuminate\Http\File;
 use Illuminate\Http\Request;
 
-    class FileService extends BaseService {
-        protected $uploadRules = [
-            'file' => 'required',
-        ];
-    
-        public function __construct(FileRepository $repository)
-        {
-            $this->repository = $repository;
-        }
+class FileService extends BaseService
+{
+    protected $uploadRules = [
+        'file' => 'required',
+    ];
 
-        public function getByName($name)
-        {
-            return $this->repository->findByName($name);
-        }
-
-        public function uploadFromRequest(Request $request) 
-        {
-            $request->validate($this->uploadRules);
-
-            $file = $request->file('file');
-            $input = [
-                'name' => $file->getClientOriginalName(),
-                'mime_type' => $file->getClientMimeType(),
-                'path' => $file->store('public/files'),
-            ];
-
-            return $this->repository->create($input);
-        }
+    public function __construct(FileRepository $repository)
+    {
+        $this->repository = $repository;
     }
+
+    public function getByName($name)
+    {
+        return $this->repository->findByName($name);
+    }
+
+    public function uploadFromRequest(Request $request)
+    {
+        $request->validate($this->uploadRules);
+
+        $file = $request->file('file');
+        $input = [
+            'name' => $file->getClientOriginalName(),
+            'mime_type' => $file->getClientMimeType(),
+            'path' => str_replace('public', 'storage', $file->store('public/files')), // TODO: rewrite file system
+        ];
+
+        return $this->repository->create($input);
+    }
+}
